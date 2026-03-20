e9041c3ec31038d9cf3621d153e31da90ec681e7
intn <me@qwqb.net>
2026-03-20 19:55:41 +0900

    config関連とかcloudflare関連

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+};
---
+++ worker/index.ts
@@ -0,0 +1,7 @@
+import type { Env } from './types';
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    return env.ASSETS.fetch(request);
+  },
+} satisfies ExportedHandler<Env>;
